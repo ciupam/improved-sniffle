c35fc6e0159d23cb877e7c688d1e7ee89c92a266
MateuszCiupa <01131212@pw.edu.pl>
2020-05-15 08:52:06 +0200

    email exists validator

---
+++ packages/server/src/modules/user/register/IsEmailAlreadyExist.ts
@@ -0,0 +1,27 @@
+import {
+  registerDecorator,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from "class-validator";
+import { User } from "../../../entity/User";
+
+@ValidatorConstraint({ async: true })
+export class IsEmailAlreadyExistConstraint
+  implements ValidatorConstraintInterface {
+  validate(email: string) {
+    return User.findOne({ where: { email } }).then((user) => !user);
+  }
+}
+
+export function IsEmailAlreadyExist(validationOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName,
+      options: validationOptions,
+      constraints: [],
+      validator: IsEmailAlreadyExistConstraint,
+    });
+  };
+}
